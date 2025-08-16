eb543941a26bc12c49228dd45e12c53dbe49c1c7
Siwali <sambuk744@gmail.com>
2025-08-16 18:00:38 +0700

    add tailwind css in detail-view

--- 3-cha-project/src/views/news/DetailVeiw.vue
+++ 3-cha-project/src/views/news/DetailVeiw.vue
@@ -10,11 +10,13 @@ const { news } = toRefs(props)
 </script>
 
 <template>
-  <div>
-    <h1>{{ news.topic }}</h1>
-    <p>Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
-    <p>By {{ news.reporter }}</p>
-    <p>{{ news.imageLink }}</p>
+  <div class="m-[200px] mt-[50px]">
+    <h1 class="text-[50px] font-bold text-left ">{{ news.topic }}</h1>
+    <p class="text-left">Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
+    <p class="text-left underline underline-offset-8 ">By {{ news.reporter }}</p>
+    <div class=" text-center m-[100px]" >
+        <img :src="news.imageLink" :alt="news.topic" class="w-[20] h-50px] rounded-md object-cover" />
+    </div>
     <p>{{ news.fullDetail }}</p>
 
 
