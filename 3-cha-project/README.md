53171a826d5488b37e7bead58b752be119d4be1b
Siwali <sambuk744@gmail.com>
2025-08-16 14:48:50 +0700

    update module

--- 3-cha-project/README.md
+++ 3-cha-project/README.md
@@ -1,4 +1,4 @@
-# 3-cha-project
+# 331-vite
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- 3-cha-project/src/main.ts
+++ 3-cha-project/src/main.ts
@@ -1,10 +1,13 @@
-import './assets/main.css'
-
+//import './assets/main.css'
+import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'nprogress/nprogress.css'
+import { inject } from '@vercel/analytics';
+inject(); 
 
 const app = createApp(App)
 
