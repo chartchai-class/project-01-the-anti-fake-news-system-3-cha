397bbfcaa376fc94cbbac57588cc879b5b46ff48
Siwali <sambuk744@gmail.com>
2025-08-16 18:45:37 +0700

    update tailwind css @DetailView

--- 3-cha-project/src/views/news/DetailVeiw.vue
+++ 3-cha-project/src/views/news/DetailVeiw.vue
@@ -12,12 +12,12 @@ const { news } = toRefs(props)
 <template>
   <div class="m-[200px] mt-[50px]">
     <h1 class="text-[50px] font-bold text-left ">{{ news.topic }}</h1>
-    <p class="text-left">Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
+    <p class="text-left mt-[-15px]">Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
     <p class="text-left underline underline-offset-8 ">By {{ news.reporter }}</p>
-    <div class=" text-center m-[100px]" >
-        <img :src="news.imageLink" :alt="news.topic" class="w-[20] h-50px] rounded-md object-cover" />
+    <div class=" text-center m-[100px] mt-[50px] mb-[50px]" >
+        <img :src="news.imageLink" :alt="news.topic" class="w-[1000px] h-[700px] text-center rounded-md object-cover" />
     </div>
-    <p>{{ news.fullDetail }}</p>
+    <p class="text-[18px] text-justify">{{ news.fullDetail }}</p>
 
 
   </div>
