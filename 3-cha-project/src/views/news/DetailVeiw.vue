24be111a5aa746e8c5bbd930cc56612137d24ff8
Siwali <sambuk744@gmail.com>
2025-08-16 19:18:11 +0700

    include VoteResultCard --> DetailView

--- 3-cha-project/src/views/news/DetailVeiw.vue
+++ 3-cha-project/src/views/news/DetailVeiw.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { toRefs} from 'vue'
 import { type New } from '@/types'
+import VoteResultCard from '@/components/VoteResultCard.vue'
 
 const props = defineProps<{
   news: New
@@ -12,13 +13,15 @@ const { news } = toRefs(props)
 <template>
   <div class="m-[200px] mt-[50px]">
     <h1 class="text-[50px] font-bold text-left ">{{ news.topic }}</h1>
-    <p class="text-left mt-[-15px]">Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
+    <p class="text-left mt-[-10px]">Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
     <p class="text-left underline underline-offset-8 ">By {{ news.reporter }}</p>
     <div class=" text-center m-[100px] mt-[50px] mb-[50px]" >
         <img :src="news.imageLink" :alt="news.topic" class="w-[1000px] h-[700px] text-center rounded-md object-cover" />
     </div>
     <p class="text-[18px] text-justify">{{ news.fullDetail }}</p>
 
-
+    <div class="mt-10 flex justify-center">
+      <VoteResultCard :votes="{ fake: news.votesFake, notFake: news.votesNotFake }" />
+    </div>
   </div>
 </template>
