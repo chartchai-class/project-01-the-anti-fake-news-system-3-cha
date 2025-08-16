da9fd7241976bdc2951c85b63ebc40aa3916629b
Siwali <sambuk744@gmail.com>
2025-08-16 16:55:52 +0700

    Revert "Merge branch 'feature/detail-view' into feature/Homepage"
    This reverts commit 550e6d078906bc95f604d6cce9a4f9a7412206fa, reversing
    changes made to 639ea948e66816ae1687ada5552000ffcc5cec47.

--- 3-cha-project/src/views/news/DetailVeiw.vue
+++ 3-cha-project/src/views/news/DetailVeiw.vue
@@ -1,22 +1,7 @@
-<script setup lang="ts">
-import { toRefs} from 'vue'
-import { type New } from '@/types'
-
-const props = defineProps<{
-  news: New
-}>()
-
-const { news } = toRefs(props)
-</script>
-
 <template>
-  <div>
-    <h1>{{ news.topic }}</h1>
-    <p>Date: {{  news.date }} | Time: {{ news.time }} | STATUS: {{ news.status }}</p>
-    <p>By {{ news.reporter }}</p>
-    <p>{{ news.imageLink }}</p>
-    <p>{{ news.fullDetail }}</p>
-
-
+  <div class="about">
+    <h1>A site for events to better the world.</h1>
   </div>
 </template>
+
+
