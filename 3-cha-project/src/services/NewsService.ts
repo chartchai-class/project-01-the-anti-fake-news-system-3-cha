36ecde40f2d264524115b5df9251e53dfec4f550
Siwali <sambuk744@gmail.com>
2025-08-17 00:04:02 +0700

    fix @NewService.ts

--- 3-cha-project/src/services/NewsService.ts
+++ 3-cha-project/src/services/NewsService.ts
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
-const apiClient = axios.create ({
-    baseURL: 'https://db-news-theta.vercel.app/',
-    withCredentials: false,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
-})
+const apiClient = axios.create({
+  baseURL: 'https://db-news-theta.vercel.app/',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
 
 export default {
-    getEvents(perPage: number, page: number) {
-        return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
-    },
-    getEvent(id: number) {
-        return apiClient.get('/events/' + id)
-    },
-}
+  getNews(perPage: number, page: number) {
+    return apiClient.get(`/api/news?_limit=${perPage}&_page=${page}`);
+  },
+  getNew(id: number) {
+    return apiClient.get(`/api/news/${id}`);
+  },
+};
