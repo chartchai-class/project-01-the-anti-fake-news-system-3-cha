b8f55ae87651afeb8479376bf98f383072b2bcf2
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-08-17 17:57:29 +0700

    create 'comment view'

---
+++ 3-cha-project/src/views/news/CommentListView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed} from 'vue'
+import { useVotesStore } from "@/stores/votes"
+import { useRoute } from "vue-router"
+
+
+const route = useRoute()
+const newsId = String(route.params.id ?? "")
+const votes = useVotesStore()
+const summary = computed(() => votes.summary(newsId))
+const totalComments = computed(() => summary.value.comments.length)
+
+
+
+</script>
+
+<template>
+  <section class="mt-10">
+
+
+    <h4 class="mt-6 mb-2 text-base font-semibold">
+      Latest comments
+    </h4>
+
+    <div v-if="totalComments === 0" class="text-sm text-gray-500">
+      No comments yet.
+    </div>
+
+  </section>
+</template>
