149aa3b54a310446ef62eb8944bb481509961aaf
Siwali <sambuk744@gmail.com>
2025-08-18 03:10:41 +0700

    update @tailwind.config

--- 3-cha-project/tailwind.config.ts
+++ 3-cha-project/tailwind.config.ts
@@ -13,18 +13,22 @@ export default {
       fontFamily: {
         sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
       },
-      // this is animmation class
+      // animation classes
       animation: {
         fade: 'yellowFade 3s ease-in-out',
+        fadeInUp: 'fadeInUp 0.8s ease-out forwards', 
       },
       keyframes: {
         yellowFade: {
           '0%': { backgroundColor: 'yellow' },
           '100%': { backgroundColor: 'transparent' },
         },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(30px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
       },
     }
   },
   plugins: [],
 } satisfies Config
-
