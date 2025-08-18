98dd0458d5c43bc9e6b73b0ae2d7a06280370030
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-08-19 01:33:14 +0700

    update tailwind

--- 3-cha-project/tailwind.config.ts
+++ 3-cha-project/tailwind.config.ts
@@ -7,28 +7,28 @@ export default {
   ],
   theme: {
     extend: {
-      boxShadow : {
-        'sp' : '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
+      boxShadow: {
+        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
       },
       fontFamily: {
         sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
       },
       // animation classes
       animation: {
-        fade: 'yellowFade 3s ease-in-out',
-        fadeInUp: 'fadeInUp 0.8s ease-out forwards', 
+        fadeInUp: 'fadeInUp 0.8s ease-out forwards',
+        fadeOutDown: 'fadeOutDown .3s ease-in both',
       },
       keyframes: {
-        yellowFade: {
-          '0%': { backgroundColor: 'yellow' },
-          '100%': { backgroundColor: 'transparent' },
-        },
         fadeInUp: {
           '0%': { opacity: '0', transform: 'translateY(30px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
+        fadeOutDown: {
+          '0%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '0', transform: 'translateY(10px)' },
+        },
       },
     }
   },
-  plugins: [], 
+  plugins: [],
 } satisfies Config
