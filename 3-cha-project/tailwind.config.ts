7f057b858eed213c7ccc396b5f5b736960592504
Siwali <sambuk744@gmail.com>
2025-08-24 22:14:25 +0700

    @css update custom css and config

--- 3-cha-project/tailwind.config.ts
+++ 3-cha-project/tailwind.config.ts
@@ -13,20 +13,39 @@ export default {
       fontFamily: {
         sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
       },
-      // animation classes
       animation: {
-        fadeInUp: 'fadeInUp 0.8s ease-out forwards',
         fadeOutDown: 'fadeOutDown .3s ease-in both',
+        slideDown: 'slideDown 0.5s ease-out forwards',
+        'fade-in-up': 'fade-in-up 0.8s ease-out forwards',
+        'slide-down': 'slide-down 0.5s ease-out forwards',
+        'slide-in': 'slideIn 0.3s ease-out forwards',
       },
       keyframes: {
-        fadeInUp: {
-          '0%': { opacity: '0', transform: 'translateY(30px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
         fadeOutDown: {
           '0%': { opacity: '1', transform: 'translateY(0)' },
           '100%': { opacity: '0', transform: 'translateY(10px)' },
         },
+        slideDown: {
+          from: { opacity: '0', transform: 'translate(-50%, -100%)' },
+          to: { opacity: '1', transform: 'translate(-50%, 0)' },
+        },
+        'fade-in-up': {
+          from: { opacity: '0', transform: 'translateY(30px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        'slide-down': {
+          from: { opacity: '0', transform: 'translate(-50%, -100%)' },
+          to: { opacity: '1', transform: 'translate(-50%, 0)' },
+        },
+        slideIn: {
+          from: { opacity: '0', transform: 'translateY(-10px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animationDelay: {
+        '100': '100ms',
+        '200': '200ms',
+        '300': '300ms',
       },
     }
   },
